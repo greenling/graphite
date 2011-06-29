0cbeeeacbb0ebe0fd78d5851913bafd222b029bd
Marcos Acosta <marcos@acosta.tv>
2011-06-29 15:47:47 -0500

    remove shifting

--- lib/graphite/client.rb
+++ lib/graphite/client.rb
@@ -15,7 +15,6 @@ module Graphite
       @prefix = prefix
       @metrics = {}
       @counters = {}
-      @shifts = {}
 
       if block_given?
         @scheduler = Rufus::Scheduler::EmScheduler.start_new
@@ -39,7 +38,6 @@ module Graphite
     end
 
     def metric(name, frequency = 1.minute, options = {})
-      add_shifts(name,options[:shifts]) if options[:shifts]
       @scheduler.every(frequency, :first_in => '1m') do
         result = yield
         log({name => result})
@@ -63,13 +61,6 @@ module Graphite
 
     private
 
-    def add_shifts(name, shifts)
-      shifts.each do |seconds|
-        @shifts[seconds] ||= []
-        @shifts[seconds] << name
-      end
-    end
-
     def log(results)
       results.keys.each do |k,v|
         @metrics["#{@prefix}.#{k}"] = results.delete(k)
@@ -87,23 +78,6 @@ module Graphite
     def send_metrics
       @logger.log(Time.now, @metrics) if @metrics.size > 0
       send_counters
-      send_shifts
-    end
-
-    # Sends metrics into the future
-    def send_shifts
-      @shifts.each do |time, metrics|
-        to_send = {}
-
-        metrics.each do |k|
-          key = "#{@prefix}.#{k}"
-          shifted_key = key + "_shifted.#{time}_ago"
-          to_send[shifted_key] = @metrics[key]
-        end
-
-        seconds = Rufus.parse_time_string time
-        @logger.log(Time.now + seconds,to_send)
-      end
     end
 
     def start_logger_timer
